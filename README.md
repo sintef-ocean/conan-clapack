6063fe6e7a73867c3ab71b713908c8224b88589e
Joakim Haugen <joakim.haugen@gmail.com>
2018-01-21 20:46:06 +0100

    shebang, TARGETS, cleaning up more, improved readme

--- README.md
+++ README.md
@@ -1,6 +1,61 @@
 [![Download](https://api.bintray.com/packages/joakimono/conan/clapack%3Ajoakimono/images/download.svg)](https://bintray.com/joakimono/conan/clapack%3Ajoakimono/_latestVersion)
 [![Build Status UNIX](https://travis-ci.org/joakimono/conan-clapack.png?branch=master)](https://travis-ci.org/joakimono/conan-clapack)
 [![Build Status WIND](https://ci.appveyor.com/api/projects/status/github/joakimono/conan-clapack?branch=master&svg=true)](https://ci.appveyor.com/project/joakimono/conan-clapack)
-# conan-clapack 
 
-clapack library package for conan.io
+
+[Conan.io](https://conan.io) recipe for [clapack](http://www.netlib.org/clapack).
+
+The recipe generates library packages, which can be found at [Bintray](https://bintray.com/joakimono/conan/clapack%3Ajoakimono).
+The package is usually consumed using the `conan install` command or a *conanfile.txt*.
+
+## How to use this package
+
+1. Add remote to conan's package [registry.txt](http://docs.conan.io/en/latest/reference/config_files/registry.txt.html):
+
+   ```bash
+   $ conan remote add joakimono https://api.bintray.com/conan/joakimono/conan
+   ```
+
+2. Using *conanfile.txt* in your project with *cmake*
+
+   Add a [*conanfile.txt*](http://docs.conan.io/en/latest/reference/conanfile_txt.html) to your project. This file describes dependencies and your configuration of choice, e.g.:
+
+   ```
+   [requires]
+   clapack/[>=3.2.1]@joakimono/stable
+
+   [options]
+   
+
+   [imports]
+   licenses, * -> ./licenses @ folder=True
+
+   [generators]
+   cmake
+   ```
+
+   Insert into your *CMakeLists.txt* something like the following lines:
+   ```cmake
+   cmake_minimum_required(VERSION 3.1.2)
+   project(TheProject CXX)
+
+   include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+   conan_basic_setup(TARGETS)
+
+   add_executable(the_executor code.cpp)
+   target_link_libraries(the_executor CONAN_PKG::clapack)
+   ```
+   Then, do
+   ```bash
+   $ mkdir build && cd build
+   $ conan install ..
+   ```
+   You can now continue with the usual dance with cmake commands for configuration and compilation. For details on how to use conan, please consult [Conan.io docs](http://docs.conan.io/en/latest/)
+
+## Package options
+
+None
+
+## Known recipe issues
+
+Not tested for mingw or cygwin on Windows.
--- conanfile.py
+++ conanfile.py
@@ -14,35 +14,36 @@ class ClapackConan(ConanFile):
     description = "CLAPACK's goal is to provide LAPACK for someone who does not have access to a Fortran compiler"
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    exports = "patch/*"
-    source_file = "clapack-3.2.1-CMAKE.tgz"
-    source_dir = source_file[:-4]
+    exports = ["patch/*"]
+    source_file = "clapack-{}-CMAKE.tgz".format(version)
+    source_subfolder = source_file[:-4]
+    build_subfolder = "build_subfolder"
     
     def source(self):
     
-        link = "http://www.netlib.org/clapack/clapack-3.2.1-CMAKE.tgz"
+        link = "http://www.netlib.org/clapack/" + self.source_file
         tools.get(link, sha1="5ea1bcc4314e392bca8b9e5f61d44355cf9f4cc1")
         
-        tools.patch(patch_file="patch/MainCMakeLists.patch", base_path=self.source_dir)
-        tools.patch(patch_file="patch/SRC_CMakeLists.patch", base_path=self.source_dir)
-        tools.patch(patch_file="patch/F2C_CMakeLists.patch", base_path=self.source_dir)
-        tools.patch(patch_file="patch/BLAS_CMakeLists.patch", base_path=self.source_dir)
-        shutil.move(self.source_dir + "/COPYING", self.source_dir + "/LICENSE")
+        tools.patch(patch_file="patch/MainCMakeLists.patch", base_path=self.source_subfolder)
+        tools.patch(patch_file="patch/SRC_CMakeLists.patch", base_path=self.source_subfolder)
+        tools.patch(patch_file="patch/F2C_CMakeLists.patch", base_path=self.source_subfolder)
+        tools.patch(patch_file="patch/BLAS_CMakeLists.patch", base_path=self.source_subfolder)
+        shutil.move(self.source_subfolder + "/COPYING", self.source_subfolder + "/LICENSE")
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder=self.source_dir)
+        cmake.configure(source_folder=self.source_subfolder, build_folder=self.build_subfolder)
         cmake.build()
         cmake.install()
 
     def package(self):
-        self.copy("COPYING", dst="licenses", src=self.source_dir, 
+        self.copy("COPYING", dst="licenses", src=self.source_subfolder,
                   ignore_case=True, keep_path=False)
 
     def package_info(self):
-        if self.settings.os == "Windows":
+        if self.settings.compiler == "Visual Studio":
             self.cpp_info.libs = ["libf2c", "blas", "lapack"]
-            if(self.settings.build_type) == "Debug":
+            if self.settings.build_type == "Debug":
                 for i in range(len(self.cpp_info.libs)):
                     self.cpp_info.libs[i] += 'd'
         else:
