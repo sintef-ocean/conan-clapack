448eaf27be974deaf1003f271c33a212b9212895
Joakim Haugen <joakim.haugen@gmail.com>
2020-06-11 08:49:11 +0200

    Add package labels and clean up code

--- conanfile.py
+++ conanfile.py
@@ -4,15 +4,19 @@
 from conans import ConanFile, CMake, tools
 import shutil
 
+
 class ClapackConan(ConanFile):
     name = "clapack"
     version = "3.2.1"
     license = "BSD 3-Clause"
-    #BSD-3-Clause-Clear
-    url = "https://github.com/joakimono/conan-clapack"
+    # BSD-3-Clause-Clear
+    url = "https://github.com/sintef-ocean/conan-clapack"
     author = "Joakim Haugen (joakim.haugen@gmail.com)"
     homepage = "http://www.netlib.org/clapack/"
-    description = "CLAPACK's goal is to provide LAPACK for someone who does not have access to a Fortran compiler"
+    description = \
+        "CLAPACK's goal is to provide LAPACK for someone who does " \
+        " not have access to a Fortran compiler"
+    topic = ("LAPACK", "Port to C", "Numerical linear algebra")
     settings = "os", "compiler", "build_type", "arch"
     generators = ("cmake_paths", "cmake_find_package")
     exports = ["patch/*"]
@@ -25,15 +29,21 @@ class ClapackConan(ConanFile):
         link = "http://www.netlib.org/clapack/" + self.source_file
         tools.get(link, sha1="5ea1bcc4314e392bca8b9e5f61d44355cf9f4cc1")
 
-        tools.patch(patch_file="patch/MainCMakeLists.patch", base_path=self.source_subfolder)
-        tools.patch(patch_file="patch/SRC_CMakeLists.patch", base_path=self.source_subfolder)
-        tools.patch(patch_file="patch/F2C_CMakeLists.patch", base_path=self.source_subfolder)
-        tools.patch(patch_file="patch/BLAS_CMakeLists.patch", base_path=self.source_subfolder)
-        shutil.move(self.source_subfolder + "/COPYING", self.source_subfolder + "/LICENSE")
+        tools.patch(patch_file="patch/MainCMakeLists.patch",
+                    base_path=self.source_subfolder)
+        tools.patch(patch_file="patch/SRC_CMakeLists.patch",
+                    base_path=self.source_subfolder)
+        tools.patch(patch_file="patch/F2C_CMakeLists.patch",
+                    base_path=self.source_subfolder)
+        tools.patch(patch_file="patch/BLAS_CMakeLists.patch",
+                    base_path=self.source_subfolder)
+        shutil.move(self.source_subfolder + "/COPYING",
+                    self.source_subfolder + "/LICENSE")
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder=self.source_subfolder, build_folder=self.build_subfolder)
+        cmake.configure(source_folder=self.source_subfolder,
+                        build_folder=self.build_subfolder)
         cmake.build()
         cmake.install()
 
--- test_package/example.cpp
+++ test_package/example.cpp
@@ -13,36 +13,36 @@ int main( )
      char JOBVT;
 
      int i;
-    
+
      integer M = SIZE;
      integer N = SIZE;
-    
+
      integer LDA = M;
      integer LDU = M;
      integer LDVT = N;
      integer LWORK;
      integer INFO;
-   
+
      integer mn = min( M, N );
-    
+
      integer MN = max( M, N );
-     
+
      double a[SIZE*SIZE] = { 16.0, 5.0, 9.0 , 4.0, 2.0, 11.0, 7.0 , 14.0, 3.0, 10.0, 6.0, 15.0, 13.0, 8.0, 12.0, 1.0};
      double s[SIZE];
      double wk[201];
      double uu[SIZE*SIZE];
      double vt[SIZE*SIZE];
-     
+
        JOBU = 'A';
-     
+
        JOBVT = 'A';
-     
+
     LWORK =  201;
-    
-    dgesvd_( &JOBU, &JOBVT, &M, &N, a, &LDA, s, uu, 
+
+    dgesvd_( &JOBU, &JOBVT, &M, &N, a, &LDA, s, uu,
           &LDU, vt, &LDVT, wk, &LWORK, &INFO);
-          
-    printf("\n INFO=%ld", INFO );          
+
+    printf("\n INFO=%ld", INFO );
 
     for ( i= 0; i< SIZE; i++ ) {
         printf("\n s[ %d ] = %f", i, s[ i ] );
