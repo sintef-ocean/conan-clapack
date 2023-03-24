f028ecd9b72741b5d927ec6186c2054d80002d98
Joakim Haugen <joakim.haugen@sintef.no>
2023-03-24 21:55:22 +0100

    Update recipe and workflow for conan 2

--- conanfile.py
+++ conanfile.py
@@ -1,78 +1,74 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from os.path import join
+from conan import ConanFile
+from conan.tools.files import get, copy
+from conan.tools.files import apply_conandata_patches, export_conandata_patches
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 
-from conans import ConanFile, CMake, tools
-import shutil
+required_conan_version = ">=1.53.0"
 
 
 class ClapackConan(ConanFile):
     name = "clapack"
     version = "3.2.1"
     license = "BSD 3-Clause"
-    # BSD-3-Clause-Clear
     url = "https://github.com/sintef-ocean/conan-clapack"
     author = "SINTEF Ocean"
     homepage = "http://www.netlib.org/clapack/"
     description = \
         "CLAPACK's goal is to provide LAPACK for someone who does " \
         "not have access to a Fortran compiler"
-    topics = ("clapack", "LAPACK", "Port to C", "Numerical linear algebra")
+    topics = ("LAPACK", "Port to C", "Numerical linear algebra")
     settings = "os", "compiler", "build_type", "arch"
+    package_type = "static-library"
     options = {
         "fPIC": [True, False],
     }
     default_options = {
         "fPIC": True,
     }
-    generators = ("cmake_paths", "cmake_find_package")
-    exports = ["patch/*"]
-    source_file = "clapack-{}-CMAKE.tgz".format(version)
-    source_subfolder = source_file[:-4]
-    build_subfolder = "build_subfolder"
+
+    def export_sources(self):
+        export_conandata_patches(self)
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        self.settings.rm_safe("compiler.libcxx")
+        self.settings.rm_safe("compiler.cppstd")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-        link = "http://www.netlib.org/clapack/" + self.source_file
-        tools.get(link, sha1="5ea1bcc4314e392bca8b9e5f61d44355cf9f4cc1")
-
-        tools.patch(patch_file="patch/MainCMakeLists.patch",
-                    base_path=self.source_subfolder)
-        tools.patch(patch_file="patch/SRC_CMakeLists.patch",
-                    base_path=self.source_subfolder)
-        tools.patch(patch_file="patch/F2C_CMakeLists.patch",
-                    base_path=self.source_subfolder)
-        tools.patch(patch_file="patch/BLAS_CMakeLists.patch",
-                    base_path=self.source_subfolder)
-        shutil.move(self.source_subfolder + "/COPYING",
-                    self.source_subfolder + "/LICENSE")
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
 
     def build(self):
+        apply_conandata_patches(self)
         cmake = CMake(self)
-        if self.settings.os != "Windows":
-            cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
-        cmake.configure(source_folder=self.source_subfolder,
-                        build_folder=self.build_subfolder)
+        cmake.configure()
         cmake.build()
-        cmake.install()
 
     def package(self):
-        self.copy("COPYING", dst="licenses", src=self.source_subfolder,
-                  ignore_case=True, keep_path=False)
+        cmake = CMake(self)
+        cmake.install()
+
+        copy(self, "COPYING", self.source_folder,
+             join(self.package_folder, "licenses"))
 
     def package_info(self):
-        self.cpp_info.name = 'CLAPACK'
-        if self.settings.compiler == "Visual Studio":
+        self.cpp_info.set_property("cmake_find_mode", "both")
+        self.cpp_info.set_property("cmake_file_name", "CLAPACK")
+        self.cpp_info.set_property("cmake_target_name", "CLAPACK::CLAPACK")
+        if self.settings.compiler == "msvc":
             self.cpp_info.libs = ["libf2c", "blas", "lapack"]
             if self.settings.build_type == "Debug":
                 for i in range(len(self.cpp_info.libs)):
                     self.cpp_info.libs[i] += 'd'
         else:
             self.cpp_info.libs = ["lapack", "blas", "f2c"]
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-
-    def configure(self):
-        del self.settings.compiler.libcxx
